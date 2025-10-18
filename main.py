9adca6dff81f3a167f5014e12df78cca71c8df31
Jeremy Nelson <jermnelson@gmail.com>
2025-10-18 15:34:03 -0600

    Adding icons for topics

--- main.py
+++ main.py
@@ -11,23 +11,24 @@ app = Application()
 app.install_router(Router, link_mode=Router.LINK_MODE_HASH)
 
 topics = [
-   {"name": "AI Agents", "stub": "ai-agents"},
-   {"name": "LLMs Hallucinations", "stub": "llms-hallucinations"},
-   {"name": "Extinction through AI", "stub": "extinction-ai"},
-   {"name": "People as AI's Eyelash Mites", "stub": "people-as-ai-eyelash-mite"},
-   {"name": "People as AI's Mitochondria", "stub": "people-as-ai-mitochondria"},
-   {"name": "Retrieval Augmented Generation (RAG)", "stub": "rag"},
-   {"name": "Knowledge Graph RAG", "stub": "graph-rag"},
-   {"name": "GLAM Knowledge Graphs", "stub": "glam-graphs"},
-   {"name": "Open Metadata", "stub": "open-metadata"},
-   {"name": "Historical Token Affirmation", "stub": "token-affirmation"},
+   {"name": "AI Agents", "stub": "ai-agents", "icon": "bi-robot"},
+   {"name": "LLMs Hallucinations", "stub": "llms-hallucinations", "icon": "bi-chat-square-dots"},
+   {"name": "Extinction through AI", "stub": "extinction-ai", "icon": "bi-emoji-dizzy-fill"},
+   {"name": "People as AI's Eyelash Mites", "stub": "people-as-ai-eyelash-mite", "icon": "bi-bug-fill"},
+   {"name": "People as AI's Mitochondria", "stub": "people-as-ai-mitochondria", "icon": "bi-battery-charging"},
+   {"name": "Retrieval Augmented Generation (RAG)", "stub": "rag", "icon": "bi-search"},
+   {"name": "Knowledge Graph RAG", "stub": "graph-rag", "icon": "bi-node-plus"},
+   {"name": "GLAM Knowledge Graphs", "stub": "glam-graphs", "icon":"bi-book"},
+   {"name": "Open Metadata", "stub": "open-metadata", "icon": "bi-patch-check"},
+   {"name": "Historical Token Affirmation", "stub": "token-affirmation", "icon": "bi-bookmark-check-fill"},
    {"name": "Introduction to Blue Core", "stub": "intro-blue-core"},
    {"name": "Graph Services", "stub": "blue-core-graph-services"},
    {"name": "Graph RAG", "stub": "blue-core-graph-rag"},
-   {"name": "AI Agents", "stub": "blue-core-ai-agents"},
+   {"name": "AI Agents", "stub": "blue-core-ai-agents", "icon": "bi-robot-fill"},
    {"name": "MCP Server", "stub": "blue-core-mcp-server"},
-    
-   {"name": "Final Thoughts", "stub": "final-thoughts"}
+   {"name": "Final Thoughts", "stub": "final-thoughts"},
+   {"name": "References", "stub": "references"}
+
 ]
 
 topic_names = {}
@@ -37,6 +38,7 @@ for topic in topics:
 home = pathlib.Path(".").parent
 docs = home / "doc"
 
+
 @t.component()
 class PresentationHead(Component):
 
@@ -45,12 +47,14 @@ class PresentationHead(Component):
             with t.div(class_name="col-2"):
                 t.img(src="static/img/dcmi-2025-home-banner.svg", style="width: 90%", alt="DCMI 2025 Logo")
                 t.h5("22 October 2025")
-            with t.div(class_name="col"):
-                t.h1("Shared Open Metadata as Critical AI Infrastructure")
-                t.h2("Jeremy Nelson, Stanford University Libraries")
+            with t.div(classes=["col", "mx-auto"]):
+                t.h1("Shared Open Metadata as Critical AI Infrastructure", style="text: center")
+                t.h2("Jeremy Nelson")
+                with t.h3():
+                    t.a("jpnelson@stanford.edu", href="mailto:jpnelson@stanford.edu")
             with t.div(class_name="col-2"):
                 with t.a(href="https://library.stanford.edu/"):
-                     t.img(src="static/img/sul-logo.png", style="width: 95%", alt="Stanford University Libraries")
+                    t.img(src="static/img/sul-logo.png", style="width: 300px;", alt="Stanford University Libraries")
         t.hr()
 
 
@@ -61,11 +65,29 @@ class PresentationFooter(Component):
         t.hr()
         with t.footer():
             with t.div(class_name="row"):
+                with t.div(class_name="col"):
+                    with t.a(href="https://library.stanford.edu/"):
+                        t.img(src="static/img/sul-logo.png", alt="Stanford University Libraries", style="width: 300px; padding: 1em;")
+                with t.div(class_name="col"):
+                    with t.a(href="https://www.dublincore.org/conferences/2025/"):
+                        t.img(src="static/img/dcmi-2025-home-banner.svg", style="width: 200px; padding: 1em;", alt="DCMI 2025 Logo")
+                    with t.ul(class_name=""):
+                         with t.li(class_name=""):
+                             t.a("Source Code", href="https://github.com/jermnelson/shared-open-metadata-as-critical-ai-infrastructure")
+                with t.div(class_name="col"):
+                    with t.a(href="https://bluecore.info/"):
+                        t.img(src="static/img/blue-core-v1.png", alt="Blue Core Website", style="width: 300px;")
+                    with t.ul(class_name=""):
+                         with t.li(class_name=""):
+                             t.a("Blue Core Website", href="https://bluecore.info/")
+                         with t.li(class_name=""):
+                             t.a("Development Blue Core", href="https://dev.bcld.info")
+                         with t.li(class_name=""):
+                             t.a("Blue Core AI Agents", href="https://github.com/blue-core-lod/bluecore-agents")
+                   
+            with t.div(class_name="row"):
                 with t.div(class_name="col-10"):
                     t.p("Copyright 2025 by Jeremy Nelson, all content licensed under CC0 with source code under Apache2")
-                with t.div(class_name="col-2"):
-                    t.img(src="static/img/qr-code.png", alt="Presentation QR code", style="width: 50px;")
-
 
 @t.component()
 class PresentationNavigation(Component):
@@ -73,34 +95,42 @@ class PresentationNavigation(Component):
     def icon(self, topic):
         if "icon" in topic:
             t.i(classes=["bi", topic["icon"]])
+        else:
+            return ""
 
     def topic_link(self, topic):
         with t.li(class_name="nav-item"):
-            t.a(topic["name"], 
-                classes=["nav-link"], 
-                href=f"#topics/{topic['stub']}") 
+            with t.a(classes=["nav-link"], 
+                     href=f"#topics/{topic['stub']}"):
+                self.icon(topic)
+                t.span(f" {topic['name']}")
+
+    def topic_list(self, topics: list):
+        for topic in topics:
+            self.topic_link(topic)
+
     
     def populate(self):
         with t.ul(classes=["nav", "flex-column"]):
             with t.li(class_name="nav-item"):
                 with t.a(class_name="nav-link", href="/"):
-                    self.icon("Home", {"icon": "bi-house-door-fill"})
+                    self.icon({"icon": "bi-house-door-fill"})
+                    t.span(" Home")
             t.li("AI Orientation & Issues", class_name="nav-item")
-            for topic in topics[0:2]:
-                self.topic_link(topic)
+            self.topic_list(topics[0:3])
             t.li("Cautious Optimism re: People & AI", class_name="nav-item")
-            for topic in topics[2:5]:
-                self.topic_link(topic)
+            self.topic_list(topics[3:5])
             t.li("RAG Medium Dive")
+            self.topic_list(topics[5:7])
             t.li("GLAM Response", class_name="nav-item")
-            for topic in topics[5:7]:
+            for topic in topics[7:10]:
                 self.topic_link(topic)
             t.li("Blue Core", class_name="nav-item")
-            for topic in topics[7:12]:
+            for topic in topics[10:14]:
                 self.topic_link(topic)
             t.li("Wrap-up", class_name="nav-item")
-            for topic in topics[12:]:
-                self.topic_link(topic)
+            self.topic_link(topics[-1])
+            
        
 
 
